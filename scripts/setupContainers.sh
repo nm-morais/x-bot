c377fc9843ba6cbbf56dc084779f287edd07d58e
Nuno Morais <nm.morais@campus.fct.unl.pt>
2021-03-04 19:29:26 +0000

    changes to  adapt to automation scripts

--- scripts/setupContainers.sh
+++ scripts/setupContainers.sh
@@ -39,53 +39,12 @@ fi
 
 echo "number of nodes: $n_nodes"
 
-currdir=$(pwd)
-delete_containers_cmd='docker ps -a | awk '{ print $1,$2 }' | grep $DOCKER_IMAGE | awk '{print $1 }' | xargs -I {} docker rm -f {}'
-build_cmd="export export LATENCY_MAP=$LATENCY_MAP; export IPS_FILE=$IPS_FILE export DOCKER_IMAGE=$DOCKER_IMAGE; cd ${currdir}; ./scripts/buildImage.sh"
-delete_logs_cmd="docker run --rm -v $SWARM_VOL:/data bash sh -c 'rm -rf /data/*'"
-host=$(hostname)
-for node in $@; do
-  {
-    echo "deleting running containers on node: $node" 
-    oarsh $node "$delete_containers_cmd"
-    echo "done deleting containers on node: $node!"  
-  } &
-done
-wait
-sleep 1s
-
-# FOR USE IN NAS
-docker run --rm -v $SWARM_VOL:/data bash sh -c 'rm -rf /data/*' || true
-# UNCOMMENT FOR LOCAL FILES ON NODES
-# for node in $@; do
-#   {
-#     echo "deleting logs on node: $node" 
-#     oarsh $node "$delete_logs_cmd" 
-#     echo "done deleting logs on node: $node!"  
-#     wait
-#   } &
-# done
-# wait
-# sleep 1s
-
-for node in $@; do
-  {
-    echo "starting build on node: $node" 
-    oarsh $node $build_cmd 
-    echo "done building image on node: $node!" 
- } &
-done
-wait
-sleep 1s
-echo "Deploying with config file:"
-cat $IPS_FILE
-
 maxcpu=$(nproc)
 nContainers=$(wc -l $IPS_FILE)
 i=0
 
 echo "Lauching containers..."
-while read -r ip
+while read -r ip name
 do
   echo "ip: $ip"
   echo "name: $name"
@@ -98,12 +57,11 @@ do
    --net $SWARM_NET \
    --ip $ip \
    --name $name \
-   -h $name \
     $DOCKER_IMAGE $i $nContainers"
 
   # echo "running command: '$cmd'"
 
   echo "Starting ${i}. Container $name with ip $ip and name $name on: $node"
-  oarsh -n $node "$cmd"
+  ssh -n $node "$cmd"
   i=$((i+1))
 done < "$IPS_FILE"
